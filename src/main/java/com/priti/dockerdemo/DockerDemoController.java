d6a7f2b986d0ad64fc7778bdaddb4084c0ee7c38
Priti Kumari <151085priti@gmail.com>
2025-04-26 22:52:15 +0800

    build docker image of application by adding dockerfile

---
+++ src/main/java/com/priti/dockerdemo/DockerDemoController.java
@@ -0,0 +1,13 @@
+package com.priti.dockerdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerDemoController {
+
+    @GetMapping("/dockerDemo")
+    public String getDemo(){
+        return "Spring Boot App is Dockerized successfully.";
+    }
+}
